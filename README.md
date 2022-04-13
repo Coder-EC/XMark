cb6c230428be7b2419231b8855ef9c7e70f321e3
Coder-EC <89246067@qq.com>
2022-04-13 17:27:32 +0800

    #2: Basic frame ui, 2022/4/13

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # XMark
+The strongest Markdown editor ever, also a good friend of programmers.
+<br>
+## Statistics & Info
+#### Project name: XMark
+#### Author: EC
+#### Code amount:
+#### Time taken: 2022/4/12 - xxx
+#### Copyright: Copyright (c) 20xx EC
+<br>
+## Features
---
+++ src/com/xmark/ui/XFrame.java
@@ -0,0 +1,23 @@
+package com.xmark.ui;
+
+import javax.swing.JFrame;
+
+/**
+ * This frame class is the base class of the frame app system. provides
+ * the basic looking, allows the content panel to display. This frame
+ * contains a self-made title bar, which has a "XMark" title on it, and
+ * the path of current file in the editor. On the right side of the title
+ * bar, omit three buttons of frame resizing: close, max and min. You
+ * don't need to pass any arguments to the constructor, because it will
+ * automatically fill in the title and the icon. The title bar will be
+ * placed in the content pane because the frame is undecorated, meaning
+ * there will be no extra border and title bar.
+ * This class extends JFrame and uses its methods.
+ *
+ * @author Coder-EC, 2022/4/13 -
+ * @version 1.0
+ * @see javax.swing.JFrame
+ */
+public class XFrame extends JFrame {
+
+}
